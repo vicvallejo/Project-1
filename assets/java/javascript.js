ba843208ec5dc2e208dad4bfaf3169fa58aea0c9
vicvallejo <76223403+vicvallejo@users.noreply.github.com>
2021-04-28 09:46:35 -0500

    Update javascript.js

--- assets/java/javascript.js
+++ assets/java/javascript.js
@@ -123,19 +123,19 @@ var fetchLyrics = document.getElementById("fetch-button");
 function getLyricsApi() {
 //get Song and Artist from LocalStorage and set them to variables to be inputs for the Lyrics API
   
-  var getLocalSong = localStorage.getItem("Song");
-  var getLocalArtist = localStorage.getItem("Artist");
-  console.log(getLocalSong);
-  console.log(getLocalArtist);
+//  var getLocalSong = localStorage.getItem("Song");
+//  var getLocalArtist = localStorage.getItem("Artist");
+//  console.log(getLocalSong);
+//  console.log(getLocalArtist);
 
   // fetch request gets Lyrics for Artist + Song requested
-  var requestUrl = `https://api.lyrics.ovh/v1/${getLocalArtist}/${getLocalSong}`;
+ // var requestUrl = `https://api.lyrics.ovh/v1/${getLocalArtist}/${getLocalSong}`;
 
-  //var word4 = setArtist + "/" + setSong;
+  var word4 = setArtist + "/" + setSong;
 
 
   // fetch request gets Lyrics for Artist + Song requested
- // var requestUrl = `https://api.lyrics.ovh/v1/` + word4;
+  var requestUrl = `https://api.lyrics.ovh/v1/` + word4;
   
   console.log(requestUrl);
 
